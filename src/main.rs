bf12de66233aa9483be8f7ef2f036d4f50972921
Shawn Lim <shawn.lim@local>
2020-02-02 17:36:57 +0800

    Add pixelating logic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,63 @@
+extern crate clap;
+extern crate image;
+use clap::{App, Arg};
+
+use image::{GenericImageView, ImageBuffer, RgbaImage};
+
+const PARAM_IN: &str = "in";
+const PARAM_OUT: &str = "out";
+const PARAM_DEGREE: &str = "degree";
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("pixelate")
+        .version("1.0")
+        .about("Pixellate supplied images")
+        .author("Shawn Lim")
+        .arg(
+            Arg::with_name(PARAM_IN)
+                .short("i")
+                .long(PARAM_IN)
+                .value_name("FILE")
+                .help("Sets a input image file")
+                .required(true),
+        )
+        .arg(
+            Arg::with_name(PARAM_OUT)
+                .short("o")
+                .long(PARAM_OUT)
+                .value_name("FILE")
+                .help("Sets a output image file")
+                .required(true),
+        )
+        .arg(
+            Arg::with_name(PARAM_DEGREE)
+                .short("d")
+                .long(PARAM_DEGREE)
+                .value_name("NUMBER")
+                .help("Sets degree pf pixelation. The higher this number, the more pixelated")
+                .required(true),
+        )
+        .get_matches();
+
+    let in_file = matches.value_of(PARAM_IN).unwrap();
+    let out_file = matches.value_of(PARAM_OUT).unwrap();
+    let d_u32 = matches
+        .value_of(PARAM_DEGREE)
+        .unwrap()
+        .parse::<u32>()
+        .unwrap();
+    let img = image::open(in_file).unwrap();
+    let (w, h) = img.dimensions();
+
+    let mut out: RgbaImage = ImageBuffer::new(w / d_u32, h / d_u32);
+    let (new_w, new_h) = out.dimensions();
+
+    for x in 0..new_w {
+        for y in 0..new_h {
+            let p = img.get_pixel(x * d_u32, y * d_u32);
+            out.put_pixel(x, y, p);
+        }
+    }
+
+    out.save(out_file).unwrap();
 }
