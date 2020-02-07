462b3eb439c7a8a3e3aabffbf58547d79ae9dfc5
Shawn Lim <shawn.lim@local>
2020-02-07 23:29:53 +0800

    Add library function

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 extern crate clap;
 extern crate image;
+extern crate pixelize;
 use clap::{App, Arg};
 
-use image::{imageops, FilterType, GenericImageView, ImageBuffer, ImageError, RgbaImage};
 use std::fs;
-use std::io::Cursor;
 
 const PARAM_IN: &str = "in";
 const PARAM_OUT: &str = "out";
@@ -50,7 +49,7 @@ fn main() {
         .unwrap();
     let buf = fs::read(in_file).unwrap();
 
-    let out = pixelize(buf.as_slice(), d).unwrap();
+    let out = pixelize::pixelize(buf.as_slice(), d).unwrap();
 
     match out.save(out_file) {
         Ok(_) => println!(
@@ -59,35 +58,4 @@ fn main() {
         ),
         Err(e) => panic!("Error pixelating your image: {}", e),
     }
-}
-
-fn pixelize(buf: &[u8], d: u32) -> Result<RgbaImage, ImageError> {
-    let format = image::guess_format(buf)?;
-    let c = Cursor::new(buf);
-    let img = image::load(c, format).unwrap();
-    let (w, h) = img.dimensions();
-    if d >= w || d >= h {
-        return Err(ImageError::UnsupportedError(String::from(
-            "degree cannot exceed the dimensions of the image!",
-        )));
-    }
-
-    if d <= 1 {
-        return Err(ImageError::UnsupportedError(String::from(
-            "degree needs to be > 1!",
-        )));
-    }
-
-    let mut out: RgbaImage = ImageBuffer::new(w / d, h / d);
-    let (new_w, new_h) = out.dimensions();
-    for x in 0..new_w {
-        for y in 0..new_h {
-            let p = img.get_pixel(x * d, y * d);
-            out.put_pixel(x, y, p);
-        }
-    }
-
-    out = imageops::resize(&out, w, h, FilterType::Nearest);
-
-    return Ok(out);
 }
