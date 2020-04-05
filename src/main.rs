7063ae67230ea6c56505f39bed44eed1f3ff9d57
Shawn Lim <shawn.lim@traveloka.com>
2020-04-05 19:01:00 +0800

    Add preserve_size option

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,10 @@ use std::fs;
 const PARAM_IN: &str = "in";
 const PARAM_OUT: &str = "out";
 const PARAM_DEGREE: &str = "degree";
+const PARAM_PRESERVE_SIZE: &str = "preserve-size";
 
 fn main() {
-    let matches = App::new("pixelate")
+  let matches = App::new("pixelate")
         .version("1.0")
         .about("Pixellate supplied images")
         .author("Shawn Lim")
@@ -38,24 +39,42 @@ fn main() {
                 .help("Sets degree of pixelation. The higher this number, the more pixelated your image will be")
                 .required(true),
         )
+        .arg(
+            Arg::with_name(PARAM_PRESERVE_SIZE)
+                .long(PARAM_PRESERVE_SIZE)
+                .value_name("BOOLEAN")
+                .help("Preserve the output size of the image")
+        )
         .get_matches();
 
-    let in_file = matches.value_of(PARAM_IN).unwrap();
-    let out_file = matches.value_of(PARAM_OUT).unwrap();
-    let d = matches
-        .value_of(PARAM_DEGREE)
+  let in_file = matches.value_of(PARAM_IN).unwrap();
+  let out_file = matches.value_of(PARAM_OUT).unwrap();
+
+  let opts = pixelize::Options {
+    degree: matches
+      .value_of(PARAM_DEGREE)
+      .unwrap()
+      .parse::<u32>()
+      .unwrap(),
+    preserve_size: if matches.is_present(PARAM_PRESERVE_SIZE) {
+      matches
+        .value_of(PARAM_PRESERVE_SIZE)
         .unwrap()
-        .parse::<u32>()
-        .unwrap();
-    let buf = fs::read(in_file).unwrap();
+        .parse::<bool>()
+        .unwrap()
+    } else {
+      true
+    },
+  };
+  let buf = fs::read(in_file).unwrap();
 
-    let out = pixelize::pixelize(buf.as_slice(), d).unwrap();
+  let out = pixelize::pixelize(buf.as_slice(), &opts).unwrap();
 
-    match out.save(out_file) {
-        Ok(_) => println!(
-            "{} pixelated with degree {} and saved to {}",
-            in_file, d, out_file
-        ),
-        Err(e) => panic!("Error pixelating your image: {}", e),
-    }
+  match out.save(out_file) {
+    Ok(_) => println!(
+      "{} pixelated with degree {} and saved to {}",
+      in_file, &opts.degree, out_file
+    ),
+    Err(e) => panic!("Error pixelating your image: {}", e),
+  }
 }
